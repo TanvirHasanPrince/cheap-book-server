d85f7a5a8675216b89f88e72ce91bd6fa6f90f8a
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2022-11-27 00:55:34 +0600

    db added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express"); 
 const cors = require("cors"); 
-const { MongoClient, ServerApiVersion } = require("mongodb"); 
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config(); 
 const port = process.env.PORT || 5000; 
@@ -8,6 +8,18 @@ const app = express();
 
 app.use(cors()); 
 app.use(express.json()); 
+ 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x1we7vi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 
 
 app.get("/", async (req, res) => {

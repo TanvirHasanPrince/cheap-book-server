865b8e755c7672f7b7f3e8d3d90e625e946b4120
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2022-11-26 14:03:35 +0600

    server on

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express"); 
+const cors = require("cors"); 
+const { MongoClient, ServerApiVersion } = require("mongodb"); 
+
+require("dotenv").config(); 
+const port = process.env.PORT || 5000; 
+const app = express(); 
+
+app.use(cors()); 
+app.use(express.json()); 
+
+
+app.get("/", async (req, res) => {
+  res.send("Book  server is running");
+});
+
+app.listen(port, () => console.log(`Book  running on ${port}`));

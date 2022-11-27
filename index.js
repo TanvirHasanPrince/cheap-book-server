ec5373ce3334cb762a86704acfe478237112783c
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2022-11-27 20:27:53 +0600

    users saved to db

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
     const categories = client.db("cheapBook").collection("bookCategories");
     const books = client.db("cheapBook").collection("BookCollection");
     const bookingsCollection = client.db("cheapBook").collection("bookings");
+    const usersCollection = client.db("cheapBook").collection("users");
 
     // API: Getting the categories in the Homepage: Start********
 
@@ -35,51 +36,65 @@ async function run() {
 
     // API: Getting the categories in the Homepage: END ********
 
-
     // API: Getting the books according to category: Start ********
 
- 
-
     app.get("/categories/fiction", async (req, res) => {
-    
       const query = { categoryName: "Fiction" };
       const booksInCategory = await books.find(query).toArray();
       res.send(booksInCategory);
     });
     app.get("/categories/fantasy", async (req, res) => {
-    
       const query = { categoryName: "Fantasy" };
       const booksInCategory = await books.find(query).toArray();
       res.send(booksInCategory);
     });
     app.get("/categories/adventure", async (req, res) => {
-    
       const query = { categoryName: "Adventure" };
       const booksInCategory = await books.find(query).toArray();
       res.send(booksInCategory);
     });
 
     // API: Getting the books according to category: End ********
-    
-
 
     // ****************START Bookings*****************
 
     //Start: API POST bookings
-    app.post('/bookings', async(req, res) => {
-      const booking = req.body
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
       console.log(booking);
       const result = await bookingsCollection.insertOne(booking);
-      res.send(result); 
-    })
+      res.send(result);
+    });
 
     //ENd: API POST bookings
 
-    // ****************END Bookings*******************
+    //START: API GET bokkings
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
 
+      const query = { email: email };
+
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
+    //END: API GET bokkings
+
+    // ****************END Bookings******** ***********
+
+
+    // *****************START USRS*********************
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+
+    })
+
+    // *****************End USRS***********************
   } finally {
   }
-} 
+}
 run().catch((error) => cosole.log(error));
 
 // APIs END*******************************************
